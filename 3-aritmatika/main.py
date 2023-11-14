71ec5072bc407ab046a16204fb49b5d9969b8df2
irfan4ami <irfan4ami@gmail.com>
2023-11-15 01:44:54 +0700

    aritmatika

---
+++ 3-aritmatika/main.py
@@ -0,0 +1,26 @@
+#input data awal
+a = int(input("\ninput data a : "))
+b = int(input("input data b : "))
+"""
+penggunaan int() jika khusus untuk angka
+penggunaan str() jika khusus di buat string
+jika tanpa str/int maka otomatis str
+"""
+print(f"\na = {a}\nb = {b}")
+
+
+#penjumlahan
+c = a + b
+print(f"penjumlahan = {c}")
+
+#pengurangan
+c = a - b
+print(f"pengurangan = {c}")
+
+#perkalian
+c = a * b
+print(f"perkalian   = {c}")
+
+#pembagian
+c = a / b
+print(f"pembagian   = {c}")
